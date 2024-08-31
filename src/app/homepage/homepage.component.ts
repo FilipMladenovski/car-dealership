c38f9f158139c89e11e8ad266ec33352aaf7af1c
FilipMladenovski <filip_chelsea@live.com>
2024-08-31 13:37:24 +0200

    homework

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit, WritableSignal, signal } from '@angular/core';
-import { PageEvent } from '@angular/material/paginator';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
 import { CarService } from '../../services/car.service';
-import { CarCardComponent } from '../car-card/car-card.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { Car } from 'app/car.model';
-import { LoaderComponent } from '../loader/loader.component';
 import { SearchFilterComponent } from '../search-filter/search-filter.component';
-import { debounceTime } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { CarCardComponent } from "../car-card/car-card.component";
 
 @Component({
   selector: 'app-homepage',
@@ -18,30 +13,30 @@ import { Subject } from 'rxjs';
   standalone: true,
   imports: [
     CommonModule,
-    MatCardModule,
-    CarCardComponent,
     SearchFilterComponent,
     MatPaginatorModule,
-    LoaderComponent,
-  ],
+    CarCardComponent
+],
 })
 export class HomepageComponent implements OnInit {
   cars: WritableSignal<Car[]> = signal<Car[]>([]);
   totalCars: WritableSignal<number> = signal<number>(0);
   page: WritableSignal<number> = signal<number>(0);
   pageSize: WritableSignal<number> = signal<number>(10);
-  searchTerm: WritableSignal<string> = signal<string>('');
-  filters: WritableSignal<any> = signal<any>({});
+  filters: WritableSignal<any> = signal<any>({
+    searchTerm: '',
+    brand: '',
+    model: '',
+    type: '',
+    minYear: null,
+    maxDistance: null,
+    minEnginePower: null,
+    transmission: '',
+    fuelType: '',
+  });
   isLoading: WritableSignal<boolean> = signal<boolean>(true);
 
-  private searchTermSubject = new Subject<string>();
-
-  constructor(private carService: CarService) {
-    this.searchTermSubject.pipe(debounceTime(300)).subscribe(term => {
-      this.searchTerm.set(term);
-      this.loadCars();
-    });
-  }
+  constructor(private carService: CarService) {}
 
   ngOnInit() {
     this.loadCars();
@@ -49,8 +44,7 @@ export class HomepageComponent implements OnInit {
 
   loadCars() {
     this.isLoading.set(true);
-    const filters = { searchTerm: this.searchTerm(), ...this.filters() };
-    this.carService.getCars(filters, this.page(), this.pageSize()).subscribe({
+    this.carService.getCars(this.filters(), this.page(), this.pageSize()).subscribe({
       next: (response) => {
         if (response && response.payload) {
           this.cars.set(response.payload);
@@ -70,8 +64,10 @@ export class HomepageComponent implements OnInit {
     });
   }
 
-  onSearchTermChanged(newTerm: string) {
-    this.searchTermSubject.next(newTerm);  // Emit the new search term with debounce
+  onFiltersChanged(updatedFilters: any) {
+    this.filters.set(updatedFilters);
+    this.page.set(0);
+    this.loadCars();
   }
 
   onPageChange(event: PageEvent) {
--- src/app/search-filter/search-filter.component.ts
+++ src/app/search-filter/search-filter.component.ts
@@ -2,21 +2,33 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-filter',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   templateUrl: './search-filter.component.html',
   styleUrls: ['./search-filter.component.css'],
 })
 export class SearchFilterComponent {
-  @Input() searchTerm: string = '';
-  @Output() searchTermChange = new EventEmitter<string>();
+  @Input() filters: any = {};
+  @Output() filtersChanged = new EventEmitter<any>();
 
-  onInputChange(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.searchTerm = value; // Update the internal model
-    this.searchTermChange.emit(value); // Emit to parent component
+  updateFilter(field: string, value: string | number): void {
+    this.filters[field] = value;
+    this.filtersChanged.emit(this.filters);
+  }
+
+  updateSelectFilter(field: string, event: any): void {
+    this.filters[field] = event.value;
+    this.filtersChanged.emit(this.filters);
   }
 }
